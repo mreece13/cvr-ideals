0d55cc25a13f52624d6a6ad9053b887ca0c55a0e
Mason Reece <mpreece@mit.edu>
2024-11-21 16:04:58 -0500

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -35,16 +35,16 @@ list(
   tar_target(data_partisan, get_data(path = PATH_PARQ, st = "COLORADO", partisan_only = TRUE, num = 100000)),
   tar_target(data_adams, filter_byCounty(data, county = "ADAMS")),
   # format data for Stan
-  tar_target(stan_data, get_stan_data(data)),
-  tar_target(stan_data_adams, get_stan_data(data_adams)),
+  tar_target(stan_data, get_stan_data(data), format="qs"),
+  tar_target(stan_data_adams, get_stan_data(data_adams), format="qs"),
   # fit `brms` models
-  tar_target(fit_bin_1pl, fit_bernoulli(data_partisan, type = "1pl")),
-  tar_target(fit_bin_2pl, fit_bernoulli(data_partisan, type = "2pl")),
+  tar_target(fit_bin_1pl, fit_bernoulli(data_partisan, type = "1pl"), format = "file"),
+  tar_target(fit_bin_2pl, fit_bernoulli(data_partisan, type = "2pl"), format = "file"),
   # fit stan models
   tar_target(stan_2pl, "R/cat_2pl.stan", format = "file"),
-  tar_target(fit_cat2pl_adams, fit_stan(stan_2pl, stan_data_adams, "cat_2pl")),
-  tar_target(fit_cat2pl_var_adams, fit_stan(stan_2pl, stan_data_adams, "cat_2pl", variational = TRUE)),
-  tar_target(fit_cat2pl_var, fit_stan(stan_2pl, stan_data, "cat_2pl", variational = TRUE))
+  tar_target(fit_cat2pl_adams, fit_stan(stan_2pl, stan_data_adams, "cat_2pl"), format = "file"),
+  tar_target(fit_cat2pl_var_adams, fit_stan(stan_2pl, stan_data_adams, "cat_2pl", variational = TRUE), format = "file"),
+  tar_target(fit_cat2pl_var, fit_stan(stan_2pl, stan_data, "cat_2pl", variational = TRUE), format = "file")
   # plots
   # tar_target(p_ber_ideals, plot_ber_ideals(fit_bin_1pl, fit_bin_2pl)),
   # tar_target(p_ber_params, plot_ber_params(fit_bin_2pl)),
