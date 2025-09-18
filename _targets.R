c5a1867e5d4d3dac84e6b8d9452b00aedca3872d
Mason Reece <mpreece@mit.edu>
2025-09-18 10:15:17 -0400

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -2,7 +2,7 @@ library(targets)
 
 source("R/functions_data.R")
 source("R/functions_fit.R")
-source("R/functions_plot.R")
+# source("R/functions_plot.R")
 
 options("brms.threads" = 20)
 options("mc.cores" = 4)
@@ -17,11 +17,6 @@ tar_option_set(
   garbage_collection = TRUE
 )
 
-tar_config_set(
-  seconds_meta_append = 15,
-  seconds_reporter = 0.5
-)
-
 username <- Sys.info()["user"]
 if (username == "mason") {
   PATH_PARQ = "../cvrs/data/pass2"
@@ -34,17 +29,17 @@ if (username == "mason") {
 list(
   # prep data
   tar_target(data, get_data(path = PATH_PARQ, st = "COLORADO", num = 100000)),
-  tar_target(data_partisan, get_data(path = PATH_PARQ, st = "COLORADO", partisan_only = TRUE, num = 100000)),
+  # tar_target(data_partisan, get_data(path = PATH_PARQ, st = "COLORADO", partisan_only = TRUE, num = 100000)),
   tar_target(data_adams, filter_byCounty(data, county = "ADAMS")),
   # format data for Stan
-  tar_target(stan_data, get_stan_data(data), format="qs"),
+  # tar_target(stan_data, get_stan_data(data), format="qs"),
   tar_target(stan_data_adams, get_stan_data(data_adams), format="qs"),
   # fit `brms` models
   # tar_target(fit_bin_1pl, fit_bernoulli(data_partisan, type = "1pl"), format = "file"),
   # tar_target(fit_bin_2pl, fit_bernoulli(data_partisan, type = "2pl"), format = "file"),
   # fit stan models
   tar_target(stan_2pl, "R/cat_2pl.stan", format = "file"),
-  tar_target(fit_cat2pl_adams, fit_stan(stan_2pl, stan_data_adams, "cat_2pl"), format = "file"),
+  tar_target(fit_cat2pl_adams, fit_stan(stan_2pl, stan_data_adams, "cat_2pl"), format = "file")
   # tar_target(fit_cat2pl_var_adams, fit_stan(stan_2pl, stan_data_adams, "cat_2pl", variational = TRUE), format = "file"),
   # tar_target(fit_cat2pl_var, fit_stan(stan_2pl, stan_data, "cat_2pl", variational = TRUE), format = "file")
   # plots
